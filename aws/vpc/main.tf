d890aa129d816b8c8cc1278e73640113786f8544
rahultiple31 <111124427+rahultiple31@users.noreply.github.com>
2025-10-23 03:55:04 -0700

    Update main.tf

--- aws/vpc/main.tf
+++ aws/vpc/main.tf
@@ -7,9 +7,7 @@ locals {
   len_private_subnets = length(var.private_subnets_cidr)
 }
 
-########
-# VPC #
-########
+# VPC
 resource "aws_vpc" "main" {
   count                = var.create_vpc ? 1 : 0
   cidr_block           = var.network_cidr
@@ -17,16 +15,10 @@ resource "aws_vpc" "main" {
   enable_dns_support   = var.enable_dns_support
   enable_dns_hostnames = var.enable_dns_hostnames
 
-  tags = merge(
-    { "Name" = var.name },
-    var.vpc_tags,
-    var.tags
-  )
+  tags = merge({ "Name" = var.name }, var.vpc_tags, var.tags)
 }
 
-########
-# Default Security Group #
-########
+# Default Security Group
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.main[0].id
 
@@ -44,15 +36,10 @@ resource "aws_default_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = merge(
-    { "Name" = "default-${var.name}" },
-    var.tags
-  )
+  tags = merge({ "Name" = "default-${var.name}" }, var.tags)
 }
 
-########
-# Public Subnets #
-########
+# Public Subnets
 resource "aws_subnet" "public" {
   count                   = local.len_public_subnets
   vpc_id                  = aws_vpc.main[0].id
@@ -60,16 +47,10 @@ resource "aws_subnet" "public" {
   availability_zone       = element(var.zones, count.index)
   map_public_ip_on_launch = true
 
-  tags = merge(
-    { "Name" = "${var.environment}-${element(var.zones, count.index)}-public" },
-    var.public_subnet_tags,
-    var.tags
-  )
+  tags = merge({ "Name" = "${var.environment}-${element(var.zones, count.index)}-public" }, var.public_subnet_tags, var.tags)
 }
 
-########
-# Private Subnets #
-########
+# Private Subnets
 resource "aws_subnet" "private" {
   count                   = local.len_private_subnets
   vpc_id                  = aws_vpc.main[0].id
@@ -77,37 +58,22 @@ resource "aws_subnet" "private" {
   availability_zone       = element(var.zones, count.index)
   map_public_ip_on_launch = var.associate_public_ip
 
-  tags = merge(
-    { "Name" = "${var.environment}-${element(var.zones, count.index)}-private" },
-    var.private_subnet_tags,
-    var.tags
-  )
+  tags = merge({ "Name" = "${var.environment}-${element(var.zones, count.index)}-private" }, var.private_subnet_tags, var.tags)
 }
 
-########
-# Internet Gateway #
-########
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   count  = var.create_internet_gateway ? 1 : 0
   vpc_id = aws_vpc.main[0].id
 
-  tags = merge(
-    { "Name" = "${var.name}-igw" },
-    var.tags
-  )
+  tags = merge({ "Name" = "${var.name}-igw" }, var.tags)
 }
 
-########
-# NAT Gateway #
-########
+# NAT Gateway
 resource "aws_eip" "nat" {
   count = var.enable_nat_gateway ? local.len_public_subnets : 0
-  # Remove 'vpc = true' as it's not supported in AWS provider >=4
 
-  tags = merge(
-    { "Name" = "${var.environment}-${element(var.zones, count.index)}-eip" },
-    var.tags
-  )
+  tags = merge({ "Name" = "${var.environment}-${element(var.zones, count.index)}-eip" }, var.tags)
 }
 
 resource "aws_nat_gateway" "nat_gw" {
@@ -115,27 +81,29 @@ resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
-  tags = merge(
-    { "Name" = "${var.environment}-${element(var.zones, count.index)}-nat" },
-    var.tags
-  )
+  tags = merge({ "Name" = "${var.environment}-${element(var.zones, count.index)}-nat" }, var.tags)
 
   depends_on = [aws_internet_gateway.igw]
 }
 
+# Public Route Table
 resource "aws_route_table" "public" {
   count  = local.len_public_subnets > 0 ? 1 : 0
   vpc_id = aws_vpc.main[0].id
   tags   = merge({ Name = "${var.environment}-public-rt" }, var.tags)
 }
+
 resource "aws_route" "public_internet" {
-  count                   = var.create_internet_gateway && local.len_public_subnets > 0 ? 1 : 0
-  route_table_id          = aws_route_table.public[0].id
-  destination_cidr_block  = "0.0.0.0/0"
-  gateway_id              = aws_internet_gateway.igw[0].id
+  count                  = var.create_internet_gateway && local.len_public_subnets > 0 ? 1 : 0
+  route_table_id         = aws_route_table.public[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw[0].id
+
   depends_on = [aws_internet_gateway.igw]
 }
+
 resource "aws_route_table_association" "public_assoc" {
   count          = local.len_public_subnets
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public[0].id
+}
